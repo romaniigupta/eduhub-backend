6b7408d2374f79489b1c09061578cf99b04c3974
romanigupta <romani.guptaa@gmail.com>
2025-05-01 11:05:34 +0530

    Replaced bcrypt with bcryptjs

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 import { User } from "../models/User.js";
-import bcrypt from "bcrypt.js";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import sendMail, { sendForgotMail } from "../middlewares/sendMail.js";
 import TryCatch from "../middlewares/TryCatch.js";
