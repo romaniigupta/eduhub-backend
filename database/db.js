55a5bba8df3192cb9a18b7b1d38bbe851c6155d1
Romani Gupta <124330790+romaniigupta@users.noreply.github.com>
2026-05-03 01:20:54 +0530

    Enhance MongoDB connection options and error logging

--- database/db.js
+++ database/db.js
@@ -1,10 +1,28 @@
+// import mongoose from "mongoose";
+
+// export const connectDb = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB);
+//     console.log("Database Connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DB);
-    console.log("Database Connected");
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+
   } catch (error) {
-    console.log(error);
+    console.error("DB ERROR:", error);
+    throw error;
   }
 };
